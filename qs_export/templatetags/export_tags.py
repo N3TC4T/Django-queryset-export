a55160357d13206a6f28580181c063cda0030da5
Ali Mohammadlo <netcat.av@gmail.com>
2016-06-25 00:19:56 +0430

    fix usage

--- qs_export/templatetags/export_tags.py
+++ qs_export/templatetags/export_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.inclusion_tag('export/exporter.html', takes_context=False)
 def exporter(btn_text, app_name, model_name ):
     """
-    Usage: {% exporter "btn text" "model_name" "app_name" %}
+    Usage: {% exporter "btn text" "app_name" "model_name" %}
     """
 
     model_name_crypt = signing.dumps(model_name)
